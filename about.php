6cb2e21749400d718afe621c80747d560e6ab040
sadaatariq-spec <sadaa.tariq@gmail.com>
2025-09-27 21:36:27 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,63 @@
+<?php
+include 'config.php';
+session_start();
+
+$user_id=$_SESSION['user_id'];
+
+if(!isset($user_id)){
+  header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Page</title>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="home.css">
+
+</head>
+<body>
+  
+<?php
+include 'user_header.php';
+?>
+<section class="about_cont">
+    <img src="about1.jpg" alt="">
+    <div class="about_descript">
+      <h2>Why Choose Us ?</h2>
+      <p>At Bookly, we offer a specialized collection of educational books covering various fields such as technology, languages, self-development, science, and management. Our goal is to provide you with the knowledge that supports your academic and professional growth.
+
+Through our online platform, you can browse books easily and securely, enjoying a smooth and user-friendly experience. Your satisfaction and trust are our top priorities.
+
+Join us and start your journey of continuous learning — because knowledge is the most valuable investment in your future.
+
+
+    </p>
+    </div>
+  </section>
+
+  <section class="questions_cont">
+    <div class="questions">
+    <h2>Have Any Queries?</h2>
+    <p>At Bookly, we value your satisfaction and strive to provide exceptional customer service. If you have any questions, concerns, or inquiries, our dedicated team is here to assist you every step of the way.</p>
+    <button class="product_btn" onclick="window.location.href='contact.php'">Contact Us</button>
+    </div>
+    
+  </section>
+
+<?php
+include 'footer.php';
+?>
+<script src="https://kit.fontawesome.com/eedbcd0c96.js" crossorigin="anonymous"></script>
+
+<script src="script.js"></script>
+
+</body>
+</html>
